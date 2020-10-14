71fd4b963cfcb73ec062686d691ef42ad51f330a
nabilkhalil <nykhalil@hotmail.com>
2020-10-14 22:53:15 +0400

    ex 12

---
+++ Ex12.R
@@ -0,0 +1,5 @@
+anscombe %>%
+  +     pivot_longer(everything(),
+                     +                  names_to = c(".value", "set"),
+                     +                  names_pattern = "(.)(.)"
+                     +     )
