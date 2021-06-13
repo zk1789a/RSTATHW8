22537c8c6faf3ddfba519cf52fb8fd6c793c595f
nykhalil21 <31440099+nykhalil21@users.noreply.github.com>
2021-06-13 18:54:44 +0400

    Add files via upload

---
+++ Shiny Dropbox.R
@@ -0,0 +1,97 @@
+# Load packages ----
+library(shiny)
+library(maps)
+library(mapproj)
+
+# Load data ---
+counties <- readRDS("/Users/nabilkhalil/Desktop/counties.rds")
+
+# Source helper functions -----
+source("helpers.R")
+percent_map <- function(var, color, legend.title, min = 0, max = 100) {
+  
+  # generate vector of fill colors for map
+  shades <- colorRampPalette(c("white", color))(100)
+  
+  # constrain gradient to percents that occur between min and max
+  var <- pmax(var, min)
+  var <- pmin(var, max)
+  percents <- as.integer(cut(var, 100, 
+                             include.lowest = TRUE, ordered = TRUE))
+  fills <- shades[percents]
+  
+  # plot choropleth map
+  map("county", fill = TRUE, col = fills, 
+      resolution = 0, lty = 0, projection = "polyconic", 
+      myborder = 0, mar = c(0,0,0,0))
+  
+  # overlay state borders
+  map("state", col = "white", fill = FALSE, add = TRUE,
+      lty = 1, lwd = 1, projection = "polyconic", 
+      myborder = 0, mar = c(0,0,0,0))
+  
+  # add a legend
+  inc <- (max - min) / 4
+  legend.text <- c(paste0(min, " % or less"),
+                   paste0(min + inc, " %"),
+                   paste0(min + 2 * inc, " %"),
+                   paste0(min + 3 * inc, " %"),
+                   paste0(max, " % or more"))
+  
+  legend("bottomleft", 
+         legend = legend.text, 
+         fill = shades[c(1, 25, 50, 75, 100)], 
+         title = legend.title)
+}
+
+# User interface ----
+ui <- fluidPage(
+  titlePanel("censusVis"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Create demographic maps with 
+        information from the 2010 US Census."),
+      
+      selectInput("var", 
+                  label = "Choose a variable to display",
+                  choices = c("Percent White", "Percent Black",
+                              "Percent Hispanic", "Percent Asian"),
+                  selected = "Percent White"),
+      
+      sliderInput("range", 
+                  label = "Range of interest:",
+                  min = 0, max = 100, value = c(0, 100))
+    ),
+    
+    mainPanel(plotOutput("map"))
+  )
+)
+
+# Server logic ----
+server <- function(input, output) {
+  output$map <- renderPlot({
+    data <- switch(input$var, 
+                   "Percent White" = counties$white,
+                   "Percent Black" = counties$black,
+                   "Percent Hispanic" = counties$hispanic,
+                   "Percent Asian" = counties$asian)
+    
+    color <- switch(input$var, 
+                    "Percent White" = "darkgreen",
+                    "Percent Black" = "black",
+                    "Percent Hispanic" = "darkorange",
+                    "Percent Asian" = "darkviolet")
+    
+    legend <- switch(input$var, 
+                     "Percent White" = "% White",
+                     "Percent Black" = "% Black",
+                     "Percent Hispanic" = "% Hispanic",
+                     "Percent Asian" = "% Asian")
+    
+    percent_map(data, color, legend, input$range[1], input$range[2])
+  })
+}
+
+# Run app ----
+shinyApp(ui, server)
